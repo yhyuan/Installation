6ac9873420a0bbf7ef670dcc504f0d688d8e1e39
Jerry Yuan <yhyuan@gmail.com>
2014-08-27 11:11:04 -0400

    -lbsd

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install -y nodejs git mongodb redis-server g++ make curl openssh-server jscoverage
+sudo apt-get install libbsd-dev
