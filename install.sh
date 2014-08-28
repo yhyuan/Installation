4ab89b369daf469bcaf08f479999ebdfd61bdb40
Jerry Yuan <yhyuan@gmail.com>
2014-08-28 19:04:46 -0400

    chromium

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install -y nodejs git mongodb redis-server g++ make curl openssh-server jscoverage
 sudo apt-get install libbsd-dev
+sudo apt-get install -y chromium-browser
