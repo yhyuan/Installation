09d31b8d12a38b121a0a7b861a79954718ccbc37
yhyuan <yhyuan@gmail.com>
2014-09-09 15:54:27 -0400

    Install gdal

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@ sudo apt-get update
 sudo apt-get install -y nodejs git mongodb redis-server g++ make curl openssh-server jscoverage
 sudo apt-get install libbsd-dev
 sudo apt-get install -y chromium-browser
+
+sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
+sudo apt-get install gdal-bin
+ogrinfo
